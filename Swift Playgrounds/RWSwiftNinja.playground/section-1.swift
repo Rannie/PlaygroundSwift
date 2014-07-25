e5e929fa6c527461ee623e50f639b2438cf15a39
Rannie <18622218653@163.com>
2014-07-25 14:13:52 +0800

    finish part one

--- Swift Playgrounds/RWSwiftNinja.playground/section-1.swift
+++ Swift Playgrounds/RWSwiftNinja.playground/section-1.swift
@@ -30,3 +30,56 @@ var s1 = ""
 flexStrings()
 flexStrings(s1: "hello", s2: "world")
 
+
+/*
+#3
+The function will return the sum of the passed parameters as a String, following the rules below.
+If a parameter is an empty string or an Int equal to 0, add -10 to the result.
+If a parameter is an String that represents a positive number (e.g. “10″, not “-5″), add it to the result.
+If a parameter is an Int, add it to the result.
+In any other case, do not add it to the result.
+*/
+
+[1, 2, 3].reduce(4) {
+    $1 + $0
+}
+
+func sumAny(anys: Any ...) -> String {
+    return String((anys.map({
+        item in
+        switch item {
+        case "" as String, 0 as Int:
+            return -10
+        case let s as String where s.toInt() > 0:
+            return s.toInt()!
+        case is Int:
+            return item as Int
+        default:
+            return 0
+        }
+        }) as [Int]).reduce(0) {
+            $0 + $1
+        }
+    )
+}
+
+let resultEmpty = sumAny()
+let result1 = sumAny(Double(), 10, "-10", 2)
+let result2 = sumAny("Marin Todorov", 2, 22, "-3", "10", "", 0, 33, -5)
+
+
+/*
+#4
+Write a function countFrom(from:Int, #to:Int) that will produce as output (eg. via print() or println()) the numbers from from to to. You can’t use any loop operators, variables, nor any built-in Array functions. Assume the value of from is less than to .
+*/
+
+func countFrom(from: Int, #to: Int) {
+    print(from) // output to the assistant editor
+    if from < to {
+        countFrom(from + 1, to: to)
+    }
+}
+
+countFrom(1, to: 5)
+
+
